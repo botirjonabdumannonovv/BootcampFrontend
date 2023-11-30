2b15d8b9c70fe7ac831392e72a93318d783b1cb9
Botirjon <thedavlativich@gmail.com>
2023-11-30 11:30:44 +0500

    update AirBnb project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,25 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./dist/*.{html,js}"],
+export default {
+  content: [
+      "./index.html",
+      "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors: {
-        textPrimary: '#222222',
-        textSecondary: '#818181',
-        logoSelected: '#000000',
-        logoSecondary: '#717171',
-        logoPrimary: '#ff385c' 
-      }
-    },
+      screens: {
+          sm: '545px',
+          md: '960px',
+          lg: '1280px',
+      },
+      extend: {
+          colors: {
+              textPrimary: '#222222',
+              textSecondary: '#818181',
+              logoSelected: '#000000',
+              logoSecondary: '#717171',
+              logoAccent: '#dddddd',
+              logoPrimary: '#ff385c',
+              borderSecondary: '#cccccc',
+          }
+      },
   },
   plugins: [],
 }
-
